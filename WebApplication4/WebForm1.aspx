340af4358c3eb107bc0d273b28fb488921dbbfbe
Gia Do <gia.dohoang@gmail.com>
2019-04-15 19:05:18 -0400

    Initial commit

---
+++ WebApplication4/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <asp:Button ID="Button1" runat="server" Text="Up Load" OnClick="Button1_Click"/>
+            <asp:Panel ID="Panel1" runat="server" 
+                BorderStyle ="Dashed" BorderColor="Blue"
+                Width="440px" >
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UploadImage();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                String fileName = FileUpload1.FileName;
+                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/Data/" + fileName));
+            }
+
+            Response.Redirect("~/WebForm1.aspx");
+        }
+
+        private void UploadImage()
+        {
+            foreach (string strFileName in Directory.GetFiles(Server.MapPath("~/Data/")))
+            {
+                ImageButton imageButton = new ImageButton();
+                FileInfo fileInfo = new FileInfo(strFileName);
+                imageButton.ImageUrl = "~/Data/" + fileInfo.Name;
+                //  imageButton.Width = Unit.Pixel(100);
+                // imageButton.Height = Unit.Pixel(100);
+                imageButton.Width = Unit.Percentage(10);
+                imageButton.Height = Unit.Percentage(10);
+                imageButton.Style.Add("padding", "5px");
+                imageButton.Click += new ImageClickEventHandler(imageButton_Click);
+                Panel1.Controls.Add(imageButton);
+            }
+        }
+
+        //THis class handle click event on the imageButton
+        private void imageButton_Click(object sender, ImageClickEventArgs e)
+        {
+
+            Response.Redirect("~/WebForm2.aspx?ImageURL=" +((ImageButton)sender).ImageUrl);
+            //throw new NotImplementedException();
+        }
+    }
+}
